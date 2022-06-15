796ab3742148e57884854dd99fcdca4eadd0e6f6
a-ryang <chnwu.kim@gmail.com>
2022-06-15 21:20:06 +0900

    Update: `server.ts` DB 초기화 추가 및 리팩토링
    - 서버 실행시 데이터베이스 init 함수 실행
    - 코드 가독성을 위해 컨테이너 함수 따로 변수에 할당

--- src/server.ts
+++ src/server.ts
@@ -3,20 +3,24 @@ import helmet from "helmet";
 import morgan from "morgan";
 import { InversifyExpressServer } from "inversify-express-utils";
 import container from "./core/container.core";
-import { Logger } from "./utils/logger.util";
 import { TYPES } from "./core/type.core";
+import { DatabaseService } from "./core/services/database.service";
+import { Logger } from "./utils/logger.util";
 
 export const server = new InversifyExpressServer(container);
 
+const loggerInstance = container.get<Logger>(TYPES.Logger);
+const databaseInstance = container.get<DatabaseService>(TYPES.IDatabaseService);
+
 server.setConfig((app) => {
+  databaseInstance.init();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(helmet());
   app.use(
     morgan("combined", {
       stream: {
-        write: (message) =>
-          container.get<Logger>(TYPES.Logger).morganStream(message),
+        write: (message) => loggerInstance.morganStream(message),
       },
     })
   );
