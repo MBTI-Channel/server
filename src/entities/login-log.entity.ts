d3fa5c04379228c1e2a6a596d7fb899c2e5cbf90
a-ryang <chnwu.kim@gmail.com>
2022-06-15 21:22:13 +0900

    Add: 엔티티

---
+++ src/entities/login-log.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  ManyToOne,
+} from "typeorm";
+
+import { User } from "./user.entity";
+
+@Entity("LoginLog")
+export class LoginLog {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn({ comment: "로그인한 시간" })
+  datetime: Date;
+
+  @Column({
+    length: "50",
+    comment: "로그인한 ip",
+  })
+  ip: string;
+
+  @Column({
+    length: "255",
+    comment: "로그인한 유저 에이전트",
+  })
+  useragent: string;
+
+  @ManyToOne(() => User, (user) => user.id, {
+    onDelete: "CASCADE",
+  })
+  user: User;
+}
