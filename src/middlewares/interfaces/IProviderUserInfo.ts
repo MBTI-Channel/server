192d1a15f5f098b9056e14fdeb4affb312c5d0d0
sktkddn777 <sktkddn777@naver.com>
2022-06-17 03:34:00 +0900

    Fix: IProviderUserInfo
    provider 필드 추가

--- src/middlewares/interfaces/IProviderUserInfo.ts
+++ src/middlewares/interfaces/IProviderUserInfo.ts
@@ -3,5 +3,5 @@ import { Provider } from "../../entities/user.entity";
 export interface IProviderUserInfo {
   provider: Provider;
   providerId: string;
-  providerData: string;
+  providerData?: string;
 }
