0c7c7674caf39b8e2b1a72f3b482f07a5b61cdd3
sktkddn777 <sktkddn777@naver.com>
2022-06-17 04:26:48 +0900

    Add: authcode route
    naver, kakao 임시 라우터 생성
    인증코드를 받는 임시 리다리엑트 라우터 생성

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,6 +3,7 @@ import { inject } from "inversify";
 import {
   BaseHttpController,
   controller,
+  httpGet,
   httpPost,
 } from "inversify-express-utils";
 import config from "../config";
@@ -32,14 +33,32 @@ export class AuthController extends BaseHttpController {
       maxAge: config.cookie.refreshTokenMaxAge,
     });
 
-    return res
-      .status(201)
-      .json({
-        nickname: user.nickname,
-        mbti: user.mbti,
-        isAdmin: user.isAdmin,
-        accessToken,
-        refreshToken,
-      });
+    return res.status(201).json({
+      nickname: user.nickname,
+      mbti: user.mbti,
+      isAdmin: user.isAdmin,
+      accessToken,
+      refreshToken,
+    });
+  }
+
+  // 임시 kakao 라우터
+  @httpGet("/kakao")
+  async kakaoLogin(req: Request, res: Response) {
+    const url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${config.naver.clientId}&redirect_uri=${config.naver.redirectUri}&state=${config.naver.randomState}`;
+    return res.redirect(url);
+  }
+
+  // 임시 naver 라우터
+  @httpGet("/naver")
+  async naverLogin(req: Request, res: Response) {
+    const url = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${config.naver.clientId}&redirect_uri=${config.naver.redirectUri}&state=${config.naver.randomState}`;
+    return res.redirect(url);
+  }
+
+  // 임시 리다이렉트 라우터
+  @httpGet("/redirect")
+  async getAuthCode(req: Request, res: Response) {
+    return res.status(200).json({ code: req.query.code });
   }
 }
