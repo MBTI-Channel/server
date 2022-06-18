508fe49cfedd73c721e413315a3467d94d0a34a7
a-ryang <chnwu.kim@gmail.com>
2022-06-18 17:13:36 +0900

    Chore: `TYPES.ts` 주석 추가

--- src/core/type.core.ts
+++ src/core/type.core.ts
@@ -2,12 +2,12 @@ export const TYPES = {
   /* utils */
   Logger: Symbol.for("Logger"),
   JwtUtil: Symbol.for("JwtUtil"),
-
   /* database */
   IDatabaseService: Symbol.for("IDatabaseService"),
+  /* auth */
   IAuthService: Symbol.for("IAuthService"),
+  /* user */
   IUserService: Symbol.for("IUserService"),
-
   /* middleware */
   GetProviderUserByOauthMiddleware: Symbol.for(
     "GetProviderUserByOauthMiddleware"
