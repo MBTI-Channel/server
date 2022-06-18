e7ac8f430ceddb24aae340aeba164fd3d38f6861
a-ryang <chnwu.kim@gmail.com>
2022-06-18 17:30:06 +0900

    Fix: `IAuth.service` 빠진 함수 추가
    - 인터페이스 작성 후 구현해야함!

--- src/modules/auth/interfaces/IAuth.service.ts
+++ src/modules/auth/interfaces/IAuth.service.ts
@@ -1,8 +1,11 @@
 import { JwtPayload } from "jsonwebtoken";
+import { User } from "../../user/entity/user.entity";
 import { LoginDto } from "../dtos/login.dto";
 
 export interface IAuthService {
   login(loginDto: LoginDto): Promise<any>;
+  generateAccessToken(user: User): Promise<string>;
+  generateRefreshToken(): Promise<string>;
 }
 
 export interface ITokenPayload extends JwtPayload {
