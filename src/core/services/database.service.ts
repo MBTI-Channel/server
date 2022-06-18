f0db1f705baf9573b0f1cb95cbf765072a45a1d9
sktkddn777 <sktkddn777@naver.com>
2022-06-18 14:51:22 +0900

    Refactor: database.service
    DB가 끊어지는 상황을 대비해 주기적으로 확인 필요

--- src/core/services/database.service.ts
+++ src/core/services/database.service.ts
@@ -10,21 +10,22 @@ export class DatabaseService implements IDatabaseService {
   private static myDataSource: DataSource;
   constructor(@inject(TYPES.Logger) private readonly logger: Logger) {}
 
-  // private async getConnection(): Promise<DataSource> {
-  //   if (DatabaseService.myDataSource?.isInitialized) {
-  //     this.logger.info("Database Connection Already Established");
-  //     return DatabaseService.myDataSource;
-  //   }
+  private async getConnection(): Promise<DataSource> {
+    if (DatabaseService.myDataSource?.isInitialized) {
+      this.logger.info("Database Connection Already Established");
+      return DatabaseService.myDataSource;
+    }
 
-  //   try {
-  //     DatabaseService.myDataSource = await appDataSource.initialize();
-  //     this.logger.info("Database Connection Established");
-  //   } catch (error) {
-  //     this.logger.error(`Database Connection Failed. Error: ${error}`);
-  //   }
+    try {
+      DatabaseService.myDataSource = await appDataSource.initialize();
+      this.logger.info("Database Connection Established");
+    } catch (error) {
+      this.logger.error(`Database Connection Failed. Error: ${error}`);
+    }
 
-  //   return DatabaseService.myDataSource;
-  // }
+    return DatabaseService.myDataSource;
+  }
+
   public async init() {
     try {
       DatabaseService.myDataSource = await appDataSource.initialize();
@@ -38,8 +39,7 @@ export class DatabaseService implements IDatabaseService {
   public async getRepository(
     entity: ObjectType<any>
   ): Promise<Repository<any>> {
-    // const connection = await this.getConnection(); // getConnection -> 연결 성공 후 앱 어디에서나 연결할 수 있음
-    // return await connection?.getRepository(entity);
-    return await DatabaseService.myDataSource?.getRepository(entity);
+    const connection = await this.getConnection(); // getConnection -> 연결 성공 후 앱 어디에서나 연결할 수 있음
+    return await connection?.getRepository(entity);
   }
 }
