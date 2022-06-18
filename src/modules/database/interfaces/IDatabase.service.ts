0259973ddee267957693893d037f5d49a01297f0
a-ryang <chnwu.kim@gmail.com>
2022-06-18 17:16:23 +0900

    Fix: `IDatabaseService` init 추가
    - 서버 실행시 DB 초기화 함수

--- src/modules/database/interfaces/IDatabase.service.ts
+++ src/modules/database/interfaces/IDatabase.service.ts
@@ -2,4 +2,5 @@ import { ObjectType, Repository } from "typeorm";
 
 export interface IDatabaseService {
   getRepository(entity: ObjectType<any>): Promise<Repository<any>>;
+  init(): Promise<void>;
 }
