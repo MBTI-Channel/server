38f3ee58ac3a327a949c88f6e9b6959c9b9b98a5
sktkddn777 <sktkddn777@naver.com>
2022-06-21 23:46:53 +0900

    Fix: iss?

--- src/modules/auth/interfaces/ITokenPayload.ts
+++ src/modules/auth/interfaces/ITokenPayload.ts
@@ -1,10 +1,9 @@
 import { JwtPayload } from "jsonwebtoken";
-import config from "../../../config";
 
 export interface ITokenPayload extends JwtPayload {
   id?: number;
   nickname?: string;
   mbti?: string;
   isAdmin?: boolean;
-  iss: string;
+  iss?: string;
 }
