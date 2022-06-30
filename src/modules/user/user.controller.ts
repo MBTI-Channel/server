c58c79d082e57f784b56256de9d0a47101fdaf9b
a-ryang <chnwu.kim@gmail.com>
2022-06-30 18:49:24 +0900

    Update: validateUser api me로 수정
    - access token 검증후 '나'에 대한 데이터를 얻는 것이므로 user/me가 맞다고 생각하여 수정

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -90,7 +90,7 @@ export class UserController extends BaseHttpController {
   }
 
   //사용자 유효한지 지속적으로 확인
-  @httpGet("/auth", TYPES.ValidateAccessTokenMiddleware)
+  @httpGet("/me", TYPES.ValidateAccessTokenMiddleware)
   async validateUser(req: Request, res: Response) {
     // TODO: access token payload의 id가 db에 존재하는지, db의 mbti, nickname과 일치하는지
 
