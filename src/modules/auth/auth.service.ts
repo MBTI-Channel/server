67a50fa92e1e309c86e5030e8c8245b89106fbf7
a-ryang <chnwu.kim@gmail.com>
2022-06-30 17:54:59 +0900

    Add: `AuthService` validateUserWithToken
    - access token의 payload가 이상이 없는지 확인하는 함수
    - payload의 id가 존재하는 user id인지, user status가 정상인지, mbti, nickname이 db와 일치한지 확인한다

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -7,12 +7,17 @@ import { TYPES } from "../../core/type.core";
 import { SignOptions } from "jsonwebtoken";
 import { JwtUtil } from "../../utils/jwt.util";
 import config from "../../config";
+import { UnauthorizedException } from "../../errors/all.exception";
+import { UserRepository } from "../user/user.repository";
+import { DecodedDto } from "./dtos/decode-token.dto";
 
 @injectable()
 export class AuthService implements IAuthService {
   constructor(
     @inject(TYPES.Logger) private readonly _logger: Logger,
-    @inject(TYPES.JwtUtil) private readonly _jwtUtil: JwtUtil
+    @inject(TYPES.JwtUtil) private readonly _jwtUtil: JwtUtil,
+    @inject(TYPES.IUserRepository)
+    private readonly _userRepository: UserRepository
   ) {}
 
   public async generateAccessToken(user: User) {
@@ -40,5 +45,40 @@ export class AuthService implements IAuthService {
     };
     const refreshToken = this._jwtUtil.sign(payload, options);
     return refreshToken;
+  }
+
+  /**
+   * payload의 데이터가 이상이 없는지 확인하는 함수
+   */
+  public async validateUserWithToken(dto: DecodedDto) {
+    this._logger.trace(`[validateUserWithToken] start`);
+
+    const { id, nickname, mbti } = dto;
+
+    const user = await this._userRepository.findOneById(id);
+
+    // 존재하지않는 user id
+    if (!user) {
+      this._logger.warn(`[validateUserWithToken] not exists user id : ${id}`);
+      throw new UnauthorizedException("authentication error");
+    }
+
+    // status가 normal이 아닌 user
+    if (user.status !== config.user.status.normal) {
+      this._logger.warn(
+        `[validateUserWithToken] not normal status user id : ${id}`
+      );
+      throw new UnauthorizedException("authentication error");
+    }
+
+    // access token payload와 db의 user 정보 불일치
+    if (user.mbti !== mbti || user.nickname !== nickname) {
+      this._logger.warn(
+        `[validateUserWithToken] access token payload and database user info does not match`
+      );
+      throw new UnauthorizedException("authentication error");
+    }
+
+    this._logger.trace(`[validateUserWithToken] clear`);
   }
 }
