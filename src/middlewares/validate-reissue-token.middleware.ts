17f6603acaed6b7e0a78a916dfa82f5f94cf1060
a-ryang <chnwu.kim@gmail.com>
2022-06-30 18:47:12 +0900

    Update: 인증 미들웨어 에러 수정, validateUserWithToken 추가
    - 코드 가독성을 위해 res 응답에서 UnauthorizedException 던지는 걸로 수정
    - ValidateAccessToken, ValidateReissueToken에 validateUserWithToken 추가하여 access token의 payload 데이터가 이상 없는지 확인

--- src/middlewares/validate-reissue-token.middleware.ts
+++ src/middlewares/validate-reissue-token.middleware.ts
@@ -2,81 +2,85 @@ import { NextFunction, Request, Response } from "express";
 import { inject, injectable } from "inversify";
 import { BaseMiddleware } from "inversify-express-utils";
 import { TYPES } from "../core/type.core";
+import { AuthService } from "../modules/auth/auth.service";
 import { JwtUtil } from "../utils/jwt.util";
 import { Logger } from "../utils/logger.util";
+import {
+  BadReqeustException,
+  UnauthorizedException,
+} from "../errors/all.exception";
 
 @injectable()
 export class ValidateReissueToken extends BaseMiddleware {
   constructor(
     @inject(TYPES.Logger) private readonly _logger: Logger,
-    @inject(TYPES.JwtUtil) private readonly _jwtUtil: JwtUtil
+    @inject(TYPES.JwtUtil) private readonly _jwtUtil: JwtUtil,
+    @inject(TYPES.IAuthService) private readonly _authService: AuthService
   ) {
     super();
   }
 
   async handler(req: Request, res: Response, next: NextFunction) {
-    this._logger.trace(`[ValidateReissueToken] start`);
-    const AUTH_TYPE = "Bearer ";
-    const header = req.headers.authorization;
+    try {
+      this._logger.trace(`[ValidateReissueToken] start`);
+      const AUTH_TYPE = "Bearer ";
+      const header = req.headers.authorization;
 
-    let accessToken: string;
-    let refreshToken: string;
+      let accessToken: string;
+      let refreshToken: string;
 
-    // access token이 authorization 헤더와 Bearer 타입인지 확인
-    if (header && header.startsWith(AUTH_TYPE)) {
-      accessToken = header.split(" ")[1];
-    } else {
-      this._logger.trace(
-        `[ValidateReissueToken] access token not in 'authorization' header or 'Bearer ' type`
-      );
-      return res.status(401).json({
-        message: "authentication error",
-      });
-    }
+      // access token이 authorization 헤더와 Bearer 타입인지 확인
+      if (header && header.startsWith(AUTH_TYPE)) {
+        accessToken = header.split(" ")[1];
+      } else {
+        this._logger.trace(
+          `[ValidateReissueToken] access token not in 'authorization' header or 'Bearer ' type`
+        );
+        throw new UnauthorizedException("authentication error");
+      }
 
-    let { status: accessTokenStatus } = this._jwtUtil.verify(accessToken);
-    // 재발급시에 access token이 만료되어야 하므로 에러
-    if (accessTokenStatus === "success") {
-      this._logger.trace(`[ValidateReissueToken] access token should expire`);
-      return res.status(400).json({
-        message: "access token should expire",
-      });
-    }
-    // access token 검증 에러
-    if (accessTokenStatus === "invalid") {
-      this._logger.trace(`[ValidateReissueToken] authentication error`);
-      return res.status(401).json({
-        message: "authentication error",
-      });
-    }
+      const { status: accessTokenStatus } = this._jwtUtil.verify(accessToken);
+      // 재발급시에 access token이 만료되어야 하므로 에러
+      if (accessTokenStatus === "success") {
+        this._logger.trace(`[ValidateReissueToken] access token should expire`);
+        throw new BadReqeustException("access token should expire");
+      }
+      // access token 검증 에러
+      if (accessTokenStatus === "invalid") {
+        this._logger.trace(`[ValidateReissueToken] authentication error`);
+        throw new UnauthorizedException("authentication error");
+      }
 
-    // refresh token이 쿠키에 있는지 확인
-    refreshToken = req.cookies.Refresh;
-    if (!refreshToken) {
-      this._logger.trace(`[ValidateReissueToken] refresh token not in cookie`);
-      return res.status(401).json({
-        message: "authentication error",
-      });
-    }
+      // access token의 payload가 유효한 사용자 확인
+      const accessTokenDecoded = this._jwtUtil.decode(accessToken);
+      await this._authService.validateUserWithToken(accessTokenDecoded!);
 
-    // refresh token 검증
-    let { status: refreshTokenStatus } = this._jwtUtil.verify(refreshToken);
-    if (refreshTokenStatus === "invalid") {
-      this._logger.trace(`[ValidateReissueToken] invald refresh token`);
-      res.clearCookie("Refresh");
-      return res.status(401).json({
-        message: "authentication error",
-      });
-    }
-    if (refreshTokenStatus === "expired") {
-      this._logger.trace(`[ValidateReissueToken] expired refresh token`);
-      res.clearCookie("Refresh");
-      return res.status(401).json({
-        message: "authentication error",
-      });
-    }
+      // refresh token이 쿠키에 있는지 확인
+      refreshToken = req.cookies.Refresh;
+      if (!refreshToken) {
+        this._logger.trace(
+          `[ValidateReissueToken] refresh token not in cookie`
+        );
+        throw new UnauthorizedException("authentication error");
+      }
 
-    this._logger.trace(`[ValidateReissueToken] call next`);
-    return next();
+      // refresh token 검증
+      const { status: refreshTokenStatus } = this._jwtUtil.verify(refreshToken);
+      if (refreshTokenStatus === "invalid") {
+        this._logger.trace(`[ValidateReissueToken] invald refresh token`);
+        res.clearCookie("Refresh");
+        throw new UnauthorizedException("authentication error");
+      }
+      if (refreshTokenStatus === "expired") {
+        this._logger.trace(`[ValidateReissueToken] expired refresh token`);
+        res.clearCookie("Refresh");
+        throw new UnauthorizedException("authentication error");
+      }
+
+      this._logger.trace(`[ValidateReissueToken] call next`);
+      return next();
+    } catch (err) {
+      return next(err);
+    }
   }
 }
