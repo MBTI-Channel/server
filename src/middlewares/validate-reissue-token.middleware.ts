d109f0b2a1501ac9f0eecfc20c265a06a7afdfe8
a-ryang <chnwu.kim@gmail.com>
2022-06-30 15:45:33 +0900

    Update: `ValidateReissueToken` 주석 추가, 변수명 수정, 검증에러 추가
    - 단계별 주석 추가
    - 블라블라TokenDecoded 보다 status값만 중요하다고 생각하여 status만 추출하는 걸로 수정
    - accessToken 검증 에러도 추가

--- src/middlewares/validate-reissue-token.middleware.ts
+++ src/middlewares/validate-reissue-token.middleware.ts
@@ -22,6 +22,7 @@ export class ValidateReissueToken extends BaseMiddleware {
     let accessToken: string;
     let refreshToken: string;
 
+    // access token이 authorization 헤더와 Bearer 타입인지 확인
     if (header && header.startsWith(AUTH_TYPE)) {
       accessToken = header.split(" ")[1];
     } else {
@@ -33,15 +34,23 @@ export class ValidateReissueToken extends BaseMiddleware {
       });
     }
 
-    let accessTokenDecoded = this._jwtUtil.verify(accessToken);
-    // refresh 재발급시에 access token이 만료되어야 하므로 에러
-    if (accessTokenDecoded.status === "success") {
+    let { status: accessTokenStatus } = this._jwtUtil.verify(accessToken);
+    // 재발급시에 access token이 만료되어야 하므로 에러
+    if (accessTokenStatus === "success") {
       this._logger.trace(`[ValidateReissueToken] access token should expire`);
       return res.status(400).json({
         message: "access token should expire",
       });
     }
+    // access token 검증 에러
+    if (accessTokenStatus === "invalid") {
+      this._logger.trace(`[ValidateReissueToken] authentication error`);
+      return res.status(401).json({
+        message: "authentication error",
+      });
+    }
 
+    // refresh token이 쿠키에 있는지 확인
     refreshToken = req.cookies.Refresh;
     if (!refreshToken) {
       this._logger.trace(`[ValidateReissueToken] refresh token not in cookie`);
@@ -50,15 +59,16 @@ export class ValidateReissueToken extends BaseMiddleware {
       });
     }
 
-    let refreshTokenDecoded = this._jwtUtil.verify(refreshToken);
-    if (refreshTokenDecoded.status === "invalid") {
+    // refresh token 검증
+    let { status: refreshTokenStatus } = this._jwtUtil.verify(refreshToken);
+    if (refreshTokenStatus === "invalid") {
       this._logger.trace(`[ValidateReissueToken] invald refresh token`);
       res.clearCookie("Refresh");
       return res.status(401).json({
         message: "authentication error",
       });
     }
-    if (refreshTokenDecoded.status === "expired") {
+    if (refreshTokenStatus === "expired") {
       this._logger.trace(`[ValidateReissueToken] expired refresh token`);
       res.clearCookie("Refresh");
       return res.status(401).json({
