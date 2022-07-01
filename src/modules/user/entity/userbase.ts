39ae85f76b224e4f9fdcfeb3ee184726bac1edaf
a-ryang <chnwu.kim@gmail.com>
2022-07-01 20:06:28 +0900

    Update: `UserBase` 생성 및 변경점 적용
    - fix: User uuid 컬럼 @Generated("uuid") 추가
    - CreateUserDto, user entity, IProviderUserInfo에 공통된 필드값들을 상위클래스  'UserBase'로 묶음
    - CreateUserDto, user entity는 이를 상속. IProviderUserInfo는 제거

---
+++ src/modules/user/entity/userbase.ts
@@ -0,0 +1,31 @@
+import { IsIn, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { Column } from "typeorm";
+import { PROVIDER_KEY } from "../../../shared/constant.shared";
+import { Provider } from "../../../shared/type.shared";
+
+export class UserBase {
+  @IsString()
+  @IsNotEmpty()
+  @IsIn(PROVIDER_KEY)
+  @Column({
+    length: "255",
+    comment: "kakao | naver",
+  })
+  provider: Provider;
+
+  @IsString()
+  @IsNotEmpty()
+  @Column({
+    length: "255",
+    comment: "소셜 계정 고유 아이디",
+  })
+  providerId: string;
+
+  @IsNumber()
+  @Column({ default: 0, type: "tinyint", comment: "0: null 1: male 2: female" })
+  gender: number;
+
+  @IsString()
+  @Column({ default: 0, length: 10, comment: "0: null" })
+  ageRange: string;
+}
--- src/shared/oauth/interfaces/IProviderUserInfo.ts
+++
@@ -1,7 +0,0 @@
-import { Provider } from "../../../modules/user/entity/user.entity";
-
-export interface IProviderUserInfo {
-  provider: Provider;
-  providerId: string;
-  providerData?: string;
-}
