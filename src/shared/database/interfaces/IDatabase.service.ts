64f7aef0784a20a56c9ad8ee900ab7a7b3d8609e
a-ryang <chnwu.kim@gmail.com>
2022-07-07 19:11:15 +0900

    Add: Database to getTransaction
    - 트랜잭션 함수(객체)를 얻는 함수

--- src/shared/database/interfaces/IDatabase.service.ts
+++ src/shared/database/interfaces/IDatabase.service.ts
@@ -1,6 +1,7 @@
-import { ObjectType, Repository } from "typeorm";
+import { ObjectType, QueryRunner, Repository } from "typeorm";
 
 export interface IDatabaseService {
   getRepository(entity: ObjectType<any>): Promise<Repository<any>>;
+  getTransaction(): Promise<QueryRunner>;
   init(): Promise<void>;
 }
