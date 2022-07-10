c33860fcfcf36cde23cb35bf5cce5dad5e25e614
a-ryang <chnwu.kim@gmail.com>
2022-07-10 18:39:02 +0900

    Add: notification entity - static typeTo, typeFrom 메소드
    - DB와 코드의 type 변환해주는 static 메소드
    - typeTo : 코드의 type을 DB로 넣을때 변환하는 메소드
    - typeFrom: DB의 type을 코드로 바꿀때 변환하는 메소드

--- src/modules/notifications/entity/notification.entity.ts
+++ src/modules/notifications/entity/notification.entity.ts
@@ -5,6 +5,7 @@ import {
   CreateDateColumn,
   ManyToOne,
 } from "typeorm";
+import { NotificationType } from "../../../shared/type.shared";
 
 import { User } from "../../user/entity/user.entity";
 
@@ -50,4 +51,36 @@ export class Notification {
     onDelete: "CASCADE",
   })
   user: User;
+
+  // db로 넣을때
+  static typeTo(type: NotificationType) {
+    switch (type) {
+      case "comment":
+        return 1;
+      case "reply":
+        return 2;
+      case "likes":
+        return 3;
+      case "trend":
+        return 4;
+      case "notice":
+        return 5;
+    }
+  }
+
+  // db에서 코드로 바꿀때
+  static typeFrom(type: number) {
+    switch (type) {
+      case 1:
+        return "comment";
+      case 2:
+        return "reply";
+      case 3:
+        return "likes";
+      case 4:
+        return "trend";
+      case 5:
+        return "notice";
+    }
+  }
 }
