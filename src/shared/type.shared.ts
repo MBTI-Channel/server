545bd51a02f0e08b216cbab251bcaedeed320d91
sktkddn777 <sktkddn777@naver.com>
2022-07-13 13:18:17 +0900

    Add: typeTo typeFrom

--- src/shared/type.shared.ts
+++ src/shared/type.shared.ts
@@ -1,4 +1,6 @@
 export type Provider = "kakao" | "naver";
+export type PostType = "post" | "mbti" | "survey" | "notice";
+export type CategoryType = "game" | "mbti" | "trip" | "love"; // 추후 카테고리 확정되면수정
 export type NotificationType =
   | "comment"
   | "reply"
