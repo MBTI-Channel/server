51f46a48757cd99fbf397948d37df1b1194eea04
a-ryang <chnwu.kim@gmail.com>
2022-07-14 16:58:37 +0900

    Rename: 댓글 모든 응답 DTO 파일로 분할
    - 응답 DTO가 길어져서 파일단위로 분할하는게 관리하는데에 있어 좋다고 생각하여 분할

--- src/modules/comment/dto/index.ts
+++ src/modules/comment/dto/index.ts
@@ -1,2 +1,4 @@
 export { CreateCommentDto } from "./create-comment-dto";
 export { DeleteCommentDto } from "./delete-comment.dto";
+export { GetAllCommentDto } from "./get-all-comment.dto";
+export { CommentResponseDto } from "./comment-response.dto";
