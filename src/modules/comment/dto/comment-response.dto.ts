68dcfafc698e2d1cb61d567369f1bc45937c00da
a-ryang <chnwu.kim@gmail.com>
2022-07-14 22:09:58 +0900

    Fix: 댓글 응답 DTO userId 추가

--- src/modules/comment/dto/comment-response.dto.ts
+++ src/modules/comment/dto/comment-response.dto.ts
@@ -3,6 +3,7 @@ import { Comment } from "../entity/comment.entity";
 
 export class CommentResponseDto {
   id: number;
+  userId: number;
   userNickname: string;
   userMbti: string;
   isSecret: boolean;
@@ -16,15 +17,16 @@ export class CommentResponseDto {
   updatedAt: Date;
 
   constructor(entity: Comment, user: User) {
-    this.id = entity.id;
-    // 미로그인 상태일 수 있기때문에 user ? 삼항연산자 사용
-    this.isMy = user ? user.isMy(entity) : false;
-    this.isPostWriter = entity.isPostWriter;
-    this.userMbti = entity.userMbti;
-    this.userNickname = entity.userNickname ?? "";
     this.isSecret = entity.isSecret;
     this.isActive = entity.isActive;
-    this.content = this.isActive ? entity.content : "";
+    this.isMy = user ? user.isMy(entity) : false;
+    this.isPostWriter = entity.isPostWriter;
+    this.id = entity.id;
+    // 미로그인 상태일 수 있기때문에 user ? 삼항연산자 사용
+    this.userId = entity.userId;
+    this.userNickname = entity.userNickname ?? "";
+    this.userMbti = entity.userMbti;
+    this.content = this.isActive === true ? entity.content : "";
     this.replyCount = entity.replyCount;
     this.likesCount = entity.likesCount;
     this.createdAt = entity.createdAt;
