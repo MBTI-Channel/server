692ff01018c20aff833ce2e57134f904b51182e8
a-ryang <chnwu.kim@gmail.com>
2022-07-21 16:52:46 +0900

    Update: Ask entity user 외래키 추가, 이메일 nullable
    - 유저 프로필에서 문의 사항 JOIN하여  확인할 수 있게 외래키 추가함
    - image -> imageUrl로 수정
    - email은 문의 답변을 선택적으로 수신할 수 있게 nullable로 수정

--- src/modules/ask/entity/ask.entity.ts
+++ src/modules/ask/entity/ask.entity.ts
@@ -1,9 +1,19 @@
-import { Entity, Column } from "typeorm";
+import { Entity, Column, ManyToOne } from "typeorm";
 import { BaseEntity } from "../../../shared/base.entity.";
+import { User } from "../../user/entity/user.entity";
 
 @Entity()
 export class Ask extends BaseEntity {
-  @Column({ comment: "문의자 이메일" })
+  // Ask (*) <-> User (1)
+  @ManyToOne(() => User, (user) => user.id, {
+    onDelete: "CASCADE",
+  })
+  user: User;
+
+  @Column({ unsigned: true })
+  userId: number;
+
+  @Column({ nullable: true, comment: "문의 알림 받을 메일" })
   email: string;
 
   @Column({ length: 100, comment: "문의 제목" })
@@ -13,7 +23,7 @@ export class Ask extends BaseEntity {
   content: string;
 
   @Column({ type: "text", comment: "문의 사진" })
-  image: string;
+  imageUrl: string;
 
   @Column({ type: "text", comment: "답변 내용" })
   answer: string;
