619d1925a5611ac8fe71017c35417f4d94aa06de
a-ryang <chnwu.kim@gmail.com>
2022-07-25 00:07:59 +0900

    Add: user agent 추출 유틸
    - 사용자 에이전트 중 의미있는 값만 추출하여 키로 활용하기 위한 유틸
    - axios, postman 같은 특수 사용자 에이전트는 이름이 짧아 그대로 return 함

---
+++ src/shared/utils/user-agent.util.ts
@@ -0,0 +1,29 @@
+import parser from "ua-parser-js";
+// .getResult() 리턴 값 예시
+// {
+//   ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+//   browser: { name: 'Chrome', version: '103.0.0.0', major: '103' },
+//   engine: { name: 'Blink', version: '103.0.0.0' },
+//   os: { name: 'Mac OS', version: '10.15.7' },
+//   device: { vendor: undefined, model: undefined, type: undefined },
+//   cpu: { architecture: undefined }
+// }
+
+const spaceReplace = (value: string[]) => {
+  return value.map((e) => e.replace(/\s+/g, "-").toLowerCase());
+};
+
+/**
+ * axios, postman 같은 경우 user-agent 길이가 짧아 그대로 반환
+ * @returns `"사용자에이전트"` || `"브라우저이름-OS이름"`
+ */
+export const converUserAgent = (userAgent: string) => {
+  const result = new parser(userAgent).getResult();
+  if (!result.browser.name || !result.os.name) return result.ua;
+
+  const [browserName, osName] = spaceReplace([
+    result.browser.name,
+    result.os.name,
+  ]);
+  return `${browserName}-${osName}`;
+};
