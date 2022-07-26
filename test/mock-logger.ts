d7f60d56daffba6be0245666522283ca3673cd85
a-ryang <chnwu.kim@gmail.com>
2022-07-26 17:34:30 +0900

    Test: 테스트코드 리팩토링
    - 기존 테스트코드 문제점이 다음이 있었음
    * inversify 컨테이너 unbind, bind 남용되어 코드 가독성이 낮았음
    * 모킹한 상수의 위치나 사용 등을 잘못했다고 생각. 가독성이 낮았음
    
    - user service의 로그인쪽은 redis 추가 적용후 작성하기
    - 나머지 테스트 코드는 이번 변경점 적용하여 작성하기

---
+++ test/mock-logger.ts
@@ -0,0 +1,6 @@
+export const mockLogger = {
+  warn: () => jest.fn(),
+  info: () => jest.fn(),
+  debug: () => jest.fn(),
+  trace: () => jest.fn(),
+};
---
+++ test/test-container.ts
@@ -0,0 +1,41 @@
+import { interfaces } from "inversify";
+import container from "../src/core/container.core";
+import { TYPES } from "../src/core/types.core";
+import { mockLogger } from "./mock-logger";
+
+export class TestContainer {
+  private _container = container;
+
+  /**
+   * 1. 각 단위 테스트가 다른 단위 테스트를 중단하지 않고 수정할 수 있도록 스냅샷 생성
+   * 2. mockLogger 리바인드
+   */
+  init() {
+    this._container.snapshot();
+    this._container.rebind(TYPES.Logger).toConstantValue(mockLogger);
+  }
+  /**
+   * 각 단위 테스트가 애플리케이션 컨테이너의 새 복사본을 가져오도록 마지막 스냅샷으로 복원
+   */
+  restore() {
+    this._container.restore();
+  }
+
+  /**
+   * `mockingImplementation`은 `Partial<T> = { [P in keyof T]?: T[P] | undefined; }`를 참고하여 작성함
+   * @param serviceIdentifier 컨테이너 식별자 `string | symbol `
+   * @param mockingInterface 모킹할 인터페이스의 구현체
+   */
+  mock<T>(
+    serviceIdentifier: interfaces.ServiceIdentifier<T>,
+    mockingInterface: { [method in keyof T]?: () => any }
+  ) {
+    this._container
+      .rebind(serviceIdentifier)
+      .toConstantValue(mockingInterface as T);
+  }
+
+  get<T>(serviceIdentifier: interfaces.ServiceIdentifier<T>): T {
+    return this._container.get<T>(serviceIdentifier);
+  }
+}
