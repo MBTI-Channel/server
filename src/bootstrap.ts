46336a159405d3e40ce7eff966da701feffe1986
a-ryang <chnwu.kim@gmail.com>
2022-07-30 19:35:46 +0900

    Add: loader.ts
    * bootstrap.ts 코드 복잡성을 낮추기 위해 앱 시작 프로세스를 관리하는 loader.ts 추가

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,34 +1,14 @@
-import express, { NextFunction, Request, Response } from "express";
-import helmet from "helmet";
-import morgan from "morgan";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import { NextFunction, Request, Response } from "express";
 import { InversifyExpressServer } from "inversify-express-utils";
 import container from "./core/container.core";
+import loader from "./core/loader.core";
 import { TYPES } from "./core/types.core";
 import { IDatabaseService } from "./core/database/interfaces/IDatabase.service";
 import { IRedisService } from "./core/database/interfaces/IRedis.service";
 import { IApiWebhookService } from "./shared/api/interfaces/IApi-webhook.service";
 import { Logger } from "./shared/utils/logger.util";
 import { HttpException } from "./shared/errors/http.exception";
-/* Swagger */
-import swaggerUi from "swagger-ui-express";
-import YAML from "yamljs";
-import { join } from "path";
 import config from "./config";
-
-type StaticOrigin = boolean | string | RegExp | (boolean | string | RegExp)[];
-const whitelist = ["http://localhost:3000"];
-const corsOptions = {
-  origin: (
-    origin: string | undefined,
-    cb: (err: Error | null, singleOrigin?: StaticOrigin) => void
-  ) => {
-    const isWhitelisted = origin && whitelist.indexOf(origin) !== -1;
-    cb(null, isWhitelisted);
-  },
-  credentials: true, // allow request with cookie
-};
 
 const loggerInstance = container.get<Logger>(TYPES.Logger);
 const apiWebhookService = container.get<IApiWebhookService>(
@@ -41,23 +21,7 @@ export async function bootstrap() {
   await container.get<IRedisService>(TYPES.IRedisService).init();
   server
     .setConfig((app) => {
-      app.set("trust proxy", true);
-      app.use(express.json());
-      app.use(express.urlencoded({ extended: true }));
-      app.use(helmet());
-      app.use(cookieParser());
-      app.use(cors(corsOptions));
-      app.use(
-        morgan("combined", {
-          stream: {
-            write: (message) => loggerInstance.morganStream(message),
-          },
-        })
-      );
-      if (process.env.NODE_ENV === "development") {
-        const swaggerYaml = YAML.load(join(__dirname, "./swagger.yaml"));
-        app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerYaml));
-      }
+      loader(app, container);
     })
     .setErrorConfig((app) => {
       app.use((req: Request, res: Response, next: NextFunction) => {
