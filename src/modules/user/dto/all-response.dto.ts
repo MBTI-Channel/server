b60c97f516b60908864fd67b60487f6cf43c1988
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-07-31 21:57:24 +0900

    Fix: UserResponseDto
    createAt 추가

--- src/modules/user/dto/all-response.dto.ts
+++ src/modules/user/dto/all-response.dto.ts
@@ -6,12 +6,14 @@ export class UserResponseDto {
   mbti: string;
   isAdmin: boolean;
   isActive: boolean;
+  createdAt: Date;
   constructor(user: User) {
     this.id = user.id;
     this.nickname = user.nickname;
     this.mbti = user.mbti;
     this.isAdmin = user.isAdmin;
     this.isActive = user.isActive();
+    this.createdAt = user.createdAt;
   }
 }
 
