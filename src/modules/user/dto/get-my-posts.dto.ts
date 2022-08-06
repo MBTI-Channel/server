a4632cf99406715b390d5333829a00249f600cc3
a-ryang <chnwu.kim@gmail.com>
2022-08-06 16:45:11 +0900

    Refactor: GetMyPostsDto PageOptionsDto를 상속으로 수정

--- src/modules/user/dto/get-my-posts.dto.ts
+++ src/modules/user/dto/get-my-posts.dto.ts
@@ -1,22 +1,4 @@
-import { Type } from "class-transformer";
-import { IsInt, IsOptional, Max, Min } from "class-validator";
+import { PageOptionsDto } from "../../../shared/page/dto/page-options.dto";
 
 // 무한스크롤로 수정 될 수 있음
-export class GetMyPostsDto {
-  @IsInt()
-  @Min(1)
-  @Type(() => Number)
-  @IsOptional()
-  page: number = 1;
-
-  @IsInt()
-  @Min(10)
-  @Max(50)
-  @Type(() => Number)
-  @IsOptional()
-  maxResults: number = 10;
-
-  get skip(): number {
-    return (this.page - 1) * this.maxResults;
-  }
-}
+export class GetMyPostsDto extends PageOptionsDto {}
