3920fdd99f9ce9243638476360367babc3c66894
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-08-08 17:38:13 +0900

    Add: increaseViewCount

--- src/modules/post/post.repository.ts
+++ src/modules/post/post.repository.ts
@@ -49,6 +49,13 @@ export class PostRepository implements IPostRepository {
     return false;
   }
 
+  public async increaseViewCount(id: number): Promise<boolean> {
+    const repository = await this._database.getRepository(Post);
+    const result = await repository.increment({ id }, "viewCount", 1);
+    if (result.affected === 1) return true;
+    return false;
+  }
+
   public async remove(id: number): Promise<void> {
     const repository = await this._database.getRepository(Post);
     await repository.update(id, {
