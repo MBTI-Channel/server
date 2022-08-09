b9db35e9b4e0b79b758046a7f645843329a8a586
a-ryang <chnwu.kim@gmail.com>
2022-08-09 23:25:20 +0900

    Add: enum MBTI

--- src/shared/enum.shared.ts
+++ src/shared/enum.shared.ts
@@ -37,3 +37,22 @@ export enum ReportTargetType { // TODO: LikeTargetType랑 중복
   POST = "post",
   COMMENT = "comment",
 }
+
+export enum Mbti {
+  ISTJ = "ISTJ",
+  ISTP = "ISTP",
+  ISFJ = "ISFJ",
+  ISFP = "ISFP",
+  INTJ = "INTJ",
+  INTP = "INTP",
+  INFJ = "INFJ",
+  INFP = "INFP",
+  ESTJ = "ESTJ",
+  ESTP = "ESTP",
+  ESFJ = "ESFJ",
+  ESFP = "ESFP",
+  ENTJ = "ENTJ",
+  ENTP = "ENTP",
+  ENFJ = "ENFJ",
+  ENFP = "ENFP",
+}
