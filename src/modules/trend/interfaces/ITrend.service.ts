40d16b50d90fc9385584a7e4d80541889e9ccf73
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-08-09 19:02:54 +0900

    Comment: TODO 추가

--- src/modules/trend/interfaces/ITrend.service.ts
+++ src/modules/trend/interfaces/ITrend.service.ts
@@ -3,5 +3,5 @@ import { Trend } from "../entity/trend.entity";
 
 export interface ITrendService {
   createTrend(targetId: number): Promise<void>;
-  getTrends(pageOptionsDto: GetAllTrendDto): Promise<any>;
+  getTrends(pageOptionsDto: GetAllTrendDto): Promise<any>; // TODO: 반환 타입 변경
 }
--- src/modules/trend/interfaces/ITrendRepository.ts
+++ src/modules/trend/interfaces/ITrendRepository.ts
@@ -3,5 +3,5 @@ import { Trend } from "../entity/trend.entity";
 
 export interface ITrendRepository {
   createTrend(trendEntity: Trend): Promise<Trend>;
-  findAllTrends(pageOptionsDto: GetAllTrendDto): Promise<any>;
+  findAllTrends(pageOptionsDto: GetAllTrendDto): Promise<any>; // TODO: 반환 타입 변경
 }
