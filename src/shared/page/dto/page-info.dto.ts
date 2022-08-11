4d0e18823d444083c9ff3f1d7866cb505f678142
a-ryang <chnwu.kim@gmail.com>
2022-08-11 18:45:37 +0900

    Update: PageInfoDto 인자값 이름에 맞게 명확하게 수정
    * totalPage 계산식에서 itemsPerPage를 사용했는데, 이름에 맞지 않으므로 maxResults 인자를 추가하여 해결

--- src/shared/page/dto/page-info.dto.ts
+++ src/shared/page/dto/page-info.dto.ts
@@ -4,11 +4,21 @@ export class PageInfoDto {
   currentPage: number;
   totalPage: number;
 
-  constructor(totalCount: number, itemsPerPage: number, currentPage: number) {
+  /**
+   * @param totalCount   전체 아이템 수
+   * @param itemsPerPage 페이지당 아이템 수
+   * @param currentPage  현재 페이지
+   * @param maxResults   페이지당 출력 아이템 수
+   */
+  constructor(
+    totalCount: number,
+    itemsPerPage: number,
+    currentPage: number,
+    maxResults: number
+  ) {
     this.totalCount = totalCount;
     this.itemsPerPage = itemsPerPage;
     this.currentPage = currentPage;
-    this.totalPage =
-      totalCount === 0 ? 1 : Math.ceil(totalCount / itemsPerPage);
+    this.totalPage = totalCount === 0 ? 1 : Math.ceil(totalCount / maxResults);
   }
 }
