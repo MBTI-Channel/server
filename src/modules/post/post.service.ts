57a2441ef90aaf844590a0a3f3147cda7ee48c39
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-08-18 23:28:27 +0900

    Fix: search 404 추가, nextId 로직 수정

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -394,8 +394,12 @@ export class PostService implements IPostService {
       }
     }
 
-    let nextId;
     if (postArray.length === 0) {
+      throw new NotFoundException(`post not found`);
+    }
+
+    let nextId: number | null;
+    if (postArray.length < pageOptionsDto.maxResults) {
       nextId = null;
     } else {
       // 배열 마지막 id를 nextId에 할당
