986ef8896afa95234580c3de737e4455950bfadd
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-09-10 01:45:33 +0900

    Add: PostDetailResponseDto

--- src/modules/post/dto/index.ts
+++ src/modules/post/dto/index.ts
@@ -1,6 +1,7 @@
 export { CreatePostDto } from "./create-post.dto";
 export { GetAllPostDto } from "./get-all-post.dto";
 export { PostResponseDto } from "./response/post-response.dto";
+export { PostDetailResponseDto } from "./response/post-detail-response.dto";
 export { SearchPostDto } from "./search-post.dto";
 export { GetMyPostsDto } from "./get-my-posts.dto";
 export { UpdatePostDto } from "./update-post.dto";
